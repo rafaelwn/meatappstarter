a2bbc53c5aca6a990b5a29362687523e3f371ac1
rafaelwn <rafaelwn@yahoo.com.br>
2017-12-03 22:13:16 -0200

    acessando um serviço mock; incluindo tratamento de erro. Iniciado a criação dos novos componentes de detalhes do restaurant.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,31 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
 import { Restaurant } from "./restaurant/restaurant.model"
 
+import { MEAT_API } from "../app.api"
+
+import { ErrorHandler } from "../app.error-handler"
+
+@Injectable()
 export class RestaurantsService {
 
-    constructor(){}
-    
-    rests: Restaurant[] = [
-        {
-            id: "bread-bakery",
-            name: "Bread & Bakery",
-            category: "Bakery",
-            deliveryEstimate: "25m",
-            rating: 4.9,
-            imagePath: "assets/img/restaurants/breadbakery.png"
-        },
-        {
-            id: "burger-house",
-            name: "MC Donalds",
-            category: "Hamburgers",
-            deliveryEstimate: "100m",
-            rating: 3.5,
-            imagePath: "assets/img/restaurants/burgerhouse.png"
-        }
+    constructor(private http: Http){}   
 
-    ]
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
 
-    restaurants(): Restaurant[] {
-        return this.rests;
     }
 
 }
